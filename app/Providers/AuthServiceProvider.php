ae9dfffb8ea690173b8e29b448543e769e540a13
debashishnit <debashish.g@endurance.com>
2017-08-25 12:24:55 +0530

    Added authentication on basis of username and auth_id to be sent in request header

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Account;
 use App\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -31,8 +32,8 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            if ($request->input('username') && $request->input('auth_id')) {
+                return Account::where('username', $request->input('username'))->where('auth_id', $request->input('auth_id'))->first();
             }
         });
     }
